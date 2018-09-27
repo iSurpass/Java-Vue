1b519cf7bf092f94038c0c0a3c8326bb7db79427
iSurpass <798051216@qq.com>
2018-09-27 20:10:09 +0800

    ljh02

---
+++ src/components/merchantlist/components/merchantListItem.vue
@@ -0,0 +1,67 @@
+<template>
+    <article class="media">
+        <div class="media-left">
+            <figure class="image is-96x96">
+                <img src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3216877797,4210662466&fm=26&gp=0.jpg">
+            </figure>
+        </div>
+        <div class="media-content" style="font-size:12px">
+            <strong>霸王龙虾</strong>
+            <div class="level is-moblie" style="margin-top:10px">
+                <div class="level-left has-text-warning">
+                    <div>
+                    <span class="icon is-small">
+                        <i class="fas fa-star"></i>
+                    </span>
+                    <span class="icon is-small">
+                        <i class="fas fa-star"></i>
+                    </span>
+                    <span class="icon is-small">
+                        <i class="fas fa-star"></i>
+                    </span>
+                    <span class="icon is-small">
+                        <i class="fas fa-star"></i>
+                    </span>
+                    <span class="icon is-small">
+                        <i class="fas fa-star"></i>
+                    </span>
+                    <span>月售515</span>
+                </div>
+                </div>
+                <div class="level-right">
+                    <span>39min</span> | <span>45.3km</span>
+                </div>
+            </div>
+            <div class="level is-moblie">
+                <div class="level-left">
+                    <span>起送价 ￥80 </span> | <span>配送费 ￥10 </span>
+                </div>
+                <div class="level-right">
+                    <span class="tag is-warning">美团专送</span>
+                </div>
+            </div>
+            <div class="level-moblie" style="margin-top:10px">
+                <div class="level-left">
+                    <img class="image is-16x16" src=">
+                    <span>本店支持开发票，开票金额100元起</span>
+                </div>
+            </div>
+        </div>
+    </article>
+</template>
+
+<script>
+export default {
+    name:"merchantListItem"
+}
+</script>
+
+<style scoped>
+.media{
+    margin: 8px;
+}
+.level{
+    margin-bottom: 0px;
+    font-size: 12px;
+}
+</style>

7ed44afd8c449652f9c1452d8b0addf74c4bdb53
bencode <bencode@163.com>
2018-09-27 20:59:11 +0800

    小改动修复下问题

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <merchantListItem />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import merchantListItem from './components/merchantlist/components/merchantListItem'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    merchantListItem
   }
 }
 </script>
